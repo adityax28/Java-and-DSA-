9decd9f08167ae21dba75c242ffdb90643aec743
Aditya Kumar <adibest1018@gmail.com>
2023-09-17 22:49:04 +0530

    new file:   ques5.java

---
+++ ques5.java
@@ -0,0 +1,42 @@
+//  https://practice.geeksforgeeks.org/problems/vowel-or-not0831/1?page=4&difficulty[]=-2&sortBy=submissions
+
+// Solution
+
+class Solution{
+    String isVowel(char c){
+        // code here
+         if(c == 'a'){
+            return "YES";
+        }
+        else if(c == 'e'){
+            return "YES";
+        }
+        else if(c == 'i'){
+            return "YES";
+        }
+        else if(c == 'o'){
+            return "YES";
+        }
+        else if(c == 'u'){
+            return "YES";
+        }
+        else if(c == 'A'){
+            return "YES";
+        }
+        else if(c == 'E'){
+            return "YES";
+        }
+        else if(c == 'I'){
+            return "YES";
+        }
+        else if(c == 'O'){
+            return "YES";
+        }
+        else if(c == 'U'){
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
+    }
+}
